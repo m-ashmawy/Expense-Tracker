a8eed9442d835e6c4412a771fbde8b01eefae320
m-ashmawy <muhammed.elashmawy@gmail.com>
2023-05-19 15:12:55 +0300

    added expenses and card components

---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+
+export default function Expenses({ expenses }) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
+      <ExpenseItem data={expenses[2]} />
+      <ExpenseItem data={expenses[3]} />
+    </Card>
+  );
+}
