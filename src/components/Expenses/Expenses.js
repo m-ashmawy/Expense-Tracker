74a4a87cd0ffe786537a89bdca3e53860cb862ab
m-ashmawy <muhammed.elashmawy@gmail.com>
2023-06-12 13:50:01 +0300

    started implementing filtering functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,8 +6,15 @@ import { useState } from "react";
 
 export default function Expenses({ expenses }) {
   const [currentYear, setCurrentYear] = useState("2019");
+  const [filteredExpenses, setFilteredExpenses] = useState(expenses);
+
   const filterChangeHandler = (filteredYear) => {
     setCurrentYear(filteredYear);
+    setFilteredExpenses(
+      expenses.filter(
+        (item) => item.date.getFullYear().toString() === filteredYear
+      )
+    );
   };
   return (
     <Card className="expenses">
@@ -15,9 +22,13 @@ export default function Expenses({ expenses }) {
         filterChangeHandler={filterChangeHandler}
         currentYear={currentYear}
       />
-      {expenses.map((expense) => (
+
+      {filteredExpenses.map((expense) => (
         <ExpenseItem key={expense.id} data={expense} />
       ))}
+      {/* {expenses.map((expense) => (
+        <ExpenseItem key={expense.id} data={expense} />
+      ))} */}
     </Card>
   );
 }
